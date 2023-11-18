8a0e6b942332648b01bb08e6663d3f8c7a254f2a
crazyfacka <gmail@joagonca.com>
2023-11-18 10:24:12 +0000

    Add proper logging

--- cec_fix.py
+++ cec_fix.py
@@ -1,8 +1,12 @@
 """Turn off TV when CEC goes berserk"""
 
+import logging
+import os
 import subprocess
 import time
 from threading import Thread
+
+logger = None
 
 last_state_time = time.time()
 current_state = 0
@@ -14,14 +18,16 @@ weird_state_machine = [
     "pwr-state: on (0x00)"
 ]
 
+def get_script_path():
+    """Returns the path of the script"""
+    return os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
 def kill_tv():
     """Turns TV off"""
+    logger.info("Killing TV in 8 seconds...")
+
     time.sleep(8)
     subprocess.run(['cec-ctl', '--to', '0', '--standby'], check=False)
-
-    with open("cec_fix.log", "a", encoding="utf-8") as file:
-        file.write(f"[{time.time()}] TV was told to sleep")
-        file.write("\n")
 
 def process_output(line):
     """Where the magic happens"""
@@ -34,10 +40,12 @@ def process_output(line):
 
     if diff >= 10:
         current_state = 0
+        logger.info("State timed out")
 
     if pretty_line == weird_state_machine[current_state]:
         last_state_time = now
         current_state += 1
+        logger.info("Moving state %d -> %d", current_state, current_state+1)
 
     if current_state == 4:
         current_state = 0
@@ -46,10 +54,25 @@ def process_output(line):
 
 def main():
     """Where the magic does not happen"""
+    global logger
+
+    logger = logging.getLogger('cec_fix')
+    logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler(f'{get_script_path()}/cec_fix.log', mode='a', encoding='utf-8')
+    fh.setLevel(logging.INFO)
+
+    log_format = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+    fh.setFormatter(log_format)
+
+    logger.addHandler(fh)
+
     process = subprocess.Popen(['cec-ctl', '-m'],
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE,
                                text=True)
+
+    logger.info("Starting to monitor CEC channel")
 
     try:
         while True:
@@ -60,7 +83,7 @@ def main():
                 time.sleep(0.1)
 
     except KeyboardInterrupt:
-        print("Stopping program")
+        logger.info("Stopping program")
 
     finally:
         process.terminate()
